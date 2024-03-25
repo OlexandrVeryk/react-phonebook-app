2dea130980d9635f5e212a6ac19d3bccd0ff2710
olexandr <114958708+OlexandrVeryk@users.noreply.github.com>
2024-03-25 11:17:49 +0000

    Added three components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Component } from "react";
-import "./App.css";
+import styles from "./App.module.css";
 import { nanoid } from "nanoid";
-import { Contacts } from "./components/Contacts/Contacts";
+import { ContactForm } from "./components/ContactForm/ContactForm";
+import { Filter } from "./components/Filter/Filter";
+import { ContactList } from "./components/ContactList/ContactList";
 
 export class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
     name: "",
     filter: "",
@@ -26,6 +28,10 @@ export class App extends Component {
     const name = form.elements.name.value;
     const number = form.elements.number.value;
 
+    if (this.state.contacts.find((contact) => contact.name === name)) {
+      return alert(`${name} is already in contacts`);
+    }
+
     this.setState((prevState) => {
       return {
         contacts: [...prevState.contacts, { id: nanoid(), name, number }],
@@ -35,7 +41,6 @@ export class App extends Component {
     form.reset();
   };
 
-
   filterContacts = () => {
     const { contacts, filter } = this.state;
     return contacts.filter((contact) =>
@@ -43,30 +48,28 @@ export class App extends Component {
     );
   };
 
+  deleteContact = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== id),
+    }));
+  };
+
   render() {
     return (
-      <>
+      <div className={styles.appContainer}>
         <h1> Phonebook </h1>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="name"
-            onChange={this.handleChange}
-            required
-            placeholder="Enter name"
-          />
-          <input
-            type="tel"
-            name="number"
-            onChange={this.handleChange}
-            required
-            placeholder="Enter number"
-            pattern="\d*"
-          />
-          <button type="submit"> Add contact</button>
-        </form>
-        <Contacts handleChange={this.handleChange} contacts={this.filterContacts}/>
-      </>
+        <ContactForm
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+        />
+        <h2>Contacts</h2>
+        <Filter handleChange={this.handleChange} />
+        <ContactList
+          contacts={this.filterContacts}
+          handleChange={this.handleChange}
+          deleteContact={this.deleteContact}
+        />
+      </div>
     );
   }
 }
